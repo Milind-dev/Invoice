0a17a3701640fefa42eadd961da60f0c68aa93e1
Milind-dev <milinddev1011@gmail.com>
2020-07-03 15:58:02 +0530

    invoice 3

--- script.js
+++ script.js
@@ -3,10 +3,15 @@
 
 
 function handleclick() {
-    var div = document.createElement('div');
+     var x = 0;
+     var count  = 0;
     var res = document.getElementById('res');
     var img = document.createElement('img');
+    var button = document.createElement('button')
+    button.textContent += 'click'
     var paid = document.createElement('p');
+
+    
     paid.textContent += 'not paid'
     paid.setAttribute('style','display:flex;color:red')
 
@@ -17,9 +22,24 @@ function handleclick() {
     p.textContent += 'santy'
     
     var money = document.createElement('p');
-    money.textContent += 0
-    res.append(img,p,money,paid) 
-    update(money);
+    money.innerHTML += 0
+    res.append(img,p,money,paid,button)
+    
+    button.addEventListener('click',function(){
+        
+        for(var i=0;i<20;i++){
+            count += i;
+            if(i>=50){
+             paid.style.color = 'green'
+             money.innerHTML += count
+            }
+            else{
+                paid.style.color = 'red'
+                money.innerHTML += count
+            }
+
+        }
+    })
 }
 window.onload = function () {
     var plusbutton = document.getElementById('adding');
